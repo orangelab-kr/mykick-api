94cd9c095f2057af49d811a2c46ab1fbe0c25be4
bakjonghun <bakjonghun@hikick.kr>
2022-01-15 02:36:33 -0500

    add: webpack for serverless deploy

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const slsw = require('serverless-webpack');
+const { isLocal } = slsw.lib.webpack;
+
+module.exports = {
+  target: 'node',
+  stats: 'normal',
+  entry: slsw.lib.entries,
+  externals: [nodeExternals()],
+  mode: isLocal ? 'development' : 'production',
+  resolve: { extensions: ['.ts', '.js'] },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    filename: '[name].js',
+    path: path.resolve(__dirname, '.webpack'),
+  },
+};
